5f12cce5b5d81bdd542820ea5714bf55836b988b
Kevin Pulido <kevinp8001@gmail.com>
2021-04-04 01:30:59 -0500

    Update TransformersNLP.py

--- TransformersNLP.py
+++ TransformersNLP.py
@@ -5,3 +5,14 @@ import wandb
 x = torch.rand(3)
 
 print(x)
+
+
+class ClassName(object):
+    '''docstring for ClassName.'''
+
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+
+    def mname(self, arg):
+        pass
